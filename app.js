ade625527e5b1ceca3f98182f96f8db5f9ba8bd9
sanjurevanthar <sanjurevanthar@gmail.com>
2026-02-07 23:58:52 -0500

    Added Workflow Controllers

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ import userRouter from "./routes/user.routes.js";
 import connectToDatabase from "./database/mongodb.js";
 import errorMiddleware from "./middlewares/error.middleware.js";
 import arcjetMiddleware from "./middlewares/arcjet.middleware.js";
+import workflowRouter from "./routes/workflow.routes.js";
 
 const app = express();
 
@@ -22,6 +23,8 @@ app.use(arcjetMiddleware);
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter );
 app.use('/api/v1/subscriptions' , subscriptionRouter);
+app.use('/api/v1/workflows' , workflowRouter);
+
 
 app.use(errorMiddleware);
 
--- config/env.js
+++ config/env.js
@@ -3,9 +3,12 @@ import {config} from "dotenv";
 config({path: `.env.${process.env.NODE_ENV || 'development'}.local`});
 
 export const {
-    PORT,
+    PORT, SERVER_URL,
     NODE_ENV,
     DB_URI,
     JWT_SECRET, JWT_EXPIRES_IN,
     ARCJET_ENV, ARCJET_KEY,
+    QSTASH_TOKEN, QSTASH_URL,
+    EMAIL_PASSWORD,
+
 } = process.env;
